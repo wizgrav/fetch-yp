5136046b6706df1e4b75c2fe76110bc358f6ff7d
Yannis Gravezas <wizgrav@gmail.com>
2023-10-19 10:28:51 +0000

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { XMLParser} from "fast-xml-parser";
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+const args = process.argv.slice(2);
+
+const parser = new XMLParser();
+
+const res = await fetch("https://dir.xiph.org/yp.xml");
+const text = await res.text();
+const obj = parser.parse(text);
+const ret = [];
+let c = 1;
+obj.directory.entry.forEach(el => {
+    ret.push({ id: c++, t: el.server_name, g: el.genre, u: el.listen_url });
+});
+fs.writeFileSync(args.length ? args[0] : 'yp.json', JSON.stringify(ret));
